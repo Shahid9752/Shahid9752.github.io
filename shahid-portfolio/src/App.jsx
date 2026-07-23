f3ce263c153c7677e8653a003d3dcc3ff9f24613
Md Shahidul Islam <shahid9752.mi@gmail.com>
2026-07-23 13:37:57 +0600

    Complete cybersecurity portfolio

---
+++ shahid-portfolio/src/App.jsx
@@ -0,0 +1,41 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Research from "./components/Research";
+import Certificates from "./components/Certificates";
+import Contact from "./components/Contact";
+function App(){
+
+return(
+
+<div>
+
+<Navbar/>
+
+<Hero/>
+
+<About/>
+
+<Skills/>
+
+<Projects/>
+
+<Experience/>
+
+<Research/>
+
+<Certificates/>
+
+<Contact/>
+
+</div>
+
+)
+
+}
+
+
+export default App;
